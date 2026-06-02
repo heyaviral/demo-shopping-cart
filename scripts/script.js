c0e01b2c2423eac87a52bc20640f6e3bb70ddd97
Aviral Dwivedi <heyaviral.tech@gmail.com>
2026-06-03 01:35:40 +0530

    First build

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,68 @@
-console.log('hello')
+let products = [
+    {
+        name: "Lay's West Indies Hot 'n' Sweet Chilli",
+        image: "/images/Products/orange-lays.webp",
+        price: 30,
+
+
+    }
+    ,
+    {
+        name: "Kellogg's Multigrain Chocos Fills Double Chocolaty",
+        image: "/images/Products/kellogs.webp",
+        price: 91,
+
+    },
+
+]
+
+let productHTML = '';
+
+
+for (let product in products) {
+    productHTML += `<div class="product-card">
+        <div class="upper-half">
+            <img src="${products[product].image}" alt="" class="logo2">
+                <hr>
+                    <p class="product-name">${products[product].name} </p>
+
+                </div>
+                <div class="lower-half">
+                    <p class="price">₹${products[product].price}</p>
+                    <button class="btn add-to-cart js-cart-button" data-name="${products[product].name}" data-price="${products[product].price}" data-image="${products[product].image}">Add</button>
+
+
+                </div>
+
+        </div> `
+}
+
+let productGrid = document.querySelector('.products-grid');
+let cartCount = document.querySelector('.cart-count');
+let cartNumber = 0;
+
+productGrid.innerHTML = productHTML;
+
+/* ADDING BUTTON LOGIC */
+
+let buttons = document.querySelectorAll('.js-cart-button');
+let gridCart = document.querySelector('.carts-grid');
+console.log(gridCart)
+
+for (let button of buttons) {
+    button.addEventListener('click', (e) => {
+        let name = button.dataset.name;
+        let price = button.dataset.price;
+        let image = button.dataset.image;
+        let info = [name, price, image];
+
+
+        cartNumber++
+
+        cartCount.innerHTML = cartNumber;
+
+
+    })
+}
+
+
